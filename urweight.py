1b320d9eaa1f9fe86d57508dbf4eb2fdb46c799d
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-04-06 15:26:05 +0530

    Weight Calculator Lbs  - Kg
    lbs to Kilogram

---
+++ urweight.py
@@ -0,0 +1,6 @@
+print(" Hi Sir, I am a weight Calculator from pounds to kg ")
+Name=input(" Enter your name ")
+print(Name)
+Weight=input(" Enter your weight in pounds ")
+Calculate=float(Weight) *0.4535924
+print(' Your Weight is '+str(Calculate) )
