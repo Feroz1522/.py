683d8e9f364c6c54583ab6cfb6bf3e69fd7ca1a3
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-04-06 14:59:21 +0530

    Getting Data's from user
    getting input from user

---
+++ data's.py
@@ -0,0 +1,10 @@
+Name = input('What is your name ')
+print(Name)
+
+Color=input('what is your favourite color ')
+print(Color)
+
+data=("hi "+ Name +" , your favourite color is " + Color )
+print(data)
+
+print(" Welcome you to this Environment " )
