1e39ef4c4a95965085da7deacad47fd3be7ef1a3
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-04-19 15:18:31 +0530

    list functions

---
+++ list.py
@@ -0,0 +1,44 @@
+'List Major Function'
+fruits_name =["apple",'orange',"grape"]
+print(fruits_name)
+
+'1'
+fruits_name.insert(0,"hello  i' m fruit")
+print(fruits_name)
+
+'2'
+fruits_name.append("pineapple")
+print(fruits_name)
+
+'3'
+fruits_name.remove("apple")
+print(fruits_name)
+
+'4'
+fruits_name.pop()
+print(fruits_name)
+
+'5'
+fruits_name.reverse()
+print(fruits_name)
+
+'6''for adding to lists'''
+Fruit_2=['goava']
+fruits_name.extend(Fruit_2)
+print(fruits_name)
+
+'7'
+print(fruits_name.index("grape"))
+print(fruits_name)
+
+'8'
+fruits_copy=fruits_name.copy()
+print(fruits_copy)
+
+'9'
+fruits_name.sort()
+print(fruits_name)
+
+'10'
+fruits_name.clear()
+print(fruits_name)
