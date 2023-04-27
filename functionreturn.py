812d67e8ebf4dab244843138851105287ebbcffb
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-04-27 12:45:06 +0530

    return Function.py
    Function using return

---
+++ functionreturn.py
@@ -0,0 +1,23 @@
+#method 1
+def cube():
+    print("enter the value ")
+    x = int(input(">")) 
+    return print(x*x*x)
+
+#method 2
+def squar(num):
+    return num * num
+ 
+#method 3
+def User_name(name):
+    return name 
+ 
+#method 4
+def user(x):
+     print("your name is " + x )
+user(x = input (">"))
+
+cube()
+print(squar(5))
+print(User_name("Feroz"))
+user(x = input (">"))
