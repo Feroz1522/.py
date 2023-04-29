1f7823132a2bea2388aea8adf20a7b890cae35ce
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-04-29 13:11:29 +0530

    Dictionary.py
    accessing dictionary using py

---
+++ dict.py
@@ -0,0 +1,8 @@
+dict_lb = {
+    "idk": "i don't know",
+    "omw": "on my way",
+    "os" :"operating system",
+    "foss" :"free and open source software"
+}
+print(dict_lb["idk"])
+print(dict_lb.get("foss"))
