58f3402236e466a1efa9981c02c2d6985b37ca1b
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-04-29 13:33:26 +0530

    Dictionary.py
    accessing dictionary library

--- dict.py
+++ dict.py
@@ -6,3 +6,12 @@ dict_lb = {
 }
 print(dict_lb["idk"])
 print(dict_lb.get("foss"))
+
+
+arts_groups = {
+    "bca":"bachelor of computer application",
+    "bcom":"bachelor of commerce",
+    "bba":"bachelor of business administration"
+}
+print(arts_groups["bba"])
+print(arts_groups.get("bca"))
