e1a06cc79d2fd94d5a8060d31037c8abec6adf83
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-05-04 15:19:44 +0530

    for loop
    basic function using for loop

---
+++ forloop.py
@@ -0,0 +1,12 @@
+user_data = ['Feroz','bca','st.joseph','techy']
+for i in user_data :
+    print(i)
+print('data has been gathered sucessfully ')
+
+x = [ 'hello', 'vanakam ',' nanbargaley',' this is my for loop']
+for z in range(0,4):
+    print (x[z])
+ 
+h = ['hello ','hi','good ',"welcome"]
+for s in h:
+    print(s)
