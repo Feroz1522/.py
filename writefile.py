8a2dee3a5958ce00c3edf4c60cd15eb1ea2754d0
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-05-06 12:35:30 +0530

    write File .py
    some basic stuffs like creating file and altering is using append

---
+++ writefile.py
@@ -0,0 +1,6 @@
+x = open("write.txt","a")
+x.write('''/n hi guyz i write this stuff using write function
+i am quite skillfull enough for gaining knowledge in this project 
+if you were interested in it you can contact me dont hesitate for contacting 
+my lap is linux i love this environment it is lovable ''')
+x.close()
